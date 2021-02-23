20630b30edd8e648332172e29d73667f663f409a
Bia Chacon <anabheatrizchacon@outlook.com>
2021-02-23 10:00:29 -0300

    start database

---
+++ api/src/controllers/UserController.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from "express";
+import { getRepository } from "typeorm";
+import { User } from "../models/User";
+
+class UserController {
+  async create(request: Request, response: Response) {
+    const { name, email } = request.body;
+    
+    const usersRepository = getRepository(User);
+
+    const userAlreadyExist = await usersRepository.findOne({email});
+
+    if(userAlreadyExist)
+      return response.status(400).json({error:"User already exists!"});
+
+    const user = usersRepository.create({
+      name,
+      email,
+    });
+
+    await usersRepository.save(user);
+
+    return response.json(user);
+  }
+}
+
+export { UserController };
--- api/src/server.ts
+++ api/src/server.ts
@@ -1,13 +1,11 @@
-import express, { request } from 'express';
+import "reflect-metadata";
+import express from "express";
+import "./database";
+import { router } from "./routes";
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({messege: "Hello Worl - NLW04"})
-});
-
-app.post("/", (request, response) => {
-  return response.json({messege: "Os dados foram salvos com sucesso!"});
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => console.log("Server is running!"));
